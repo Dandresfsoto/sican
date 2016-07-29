67ee4dd5e804b848abe088795f709d7c226193cd
Dandresfsoto <dandresfsoto@gmail.com>
2016-07-29 11:20:44 -0500

    Modelo docentes min educacion unique and null

--- docentes/models.py
+++ docentes/models.py
@@ -7,7 +7,7 @@ from radicados.models import Radicado
 
 # Create your models here.
 class DocentesMinEducacion(models.Model):
-    cedula = models.BigIntegerField()
+    cedula = models.BigIntegerField(unique=True)
     primer_apellido = models.CharField(max_length=100)
     segundo_apellido = models.CharField(max_length=100,blank=True)
     primer_nombre = models.CharField(max_length=100)
@@ -16,9 +16,9 @@ class DocentesMinEducacion(models.Model):
     correo = models.EmailField(max_length=100,blank=True)
     telefono_fijo = models.BigIntegerField(blank=True)
     telefono_celular = models.BigIntegerField(blank=True)
-    departamento = models.ForeignKey(Departamento,blank=True)
-    municipio = models.ForeignKey(Municipio,blank=True)
-    radicado = models.ForeignKey(Radicado,blank=True)
+    departamento = models.ForeignKey(Departamento,blank=True,null=True)
+    municipio = models.ForeignKey(Municipio,blank=True,null=True)
+    radicado = models.ForeignKey(Radicado,blank=True,null=True)
 
     class Meta:
         ordering = ['primer_apellido']
