ef531c8f90b40045922919e41890d9af78d82675
Dandresfs <dandresfsoto@gmail.com>
2016-10-04 15:42:54 -0500

    Red bug 3

--- evidencias/tasks.py
+++ evidencias/tasks.py
@@ -74,7 +74,7 @@ def build_red(id_red):
         inicia = 2
 
 
-    beneficiarios_id = red.evidencias.values_list('beneficiarios_cargados__id',flat=True)
+    beneficiarios_id = red.evidencias.values_list('beneficiarios_cargados__id',flat=True).distinct()
 
 
     i = 0 + inicia
