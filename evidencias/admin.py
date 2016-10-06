780ad378bc0d9999510cb7866cb5bb92e8f56b99
Dandresfs <dandresfsoto@gmail.com>
2016-10-06 08:23:01 -0500

    Carga masiva admin

--- evidencias/admin.py
+++ evidencias/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from evidencias.models import Evidencia, Red, CargaMasiva
+from evidencias.tasks import carga_masiva_evidencias
 # Register your models here.
 
 admin.site.register(Evidencia)
 admin.site.register(Red)
-admin.site.register(CargaMasiva)
+
+def masivo(modeladmin, request, queryset):
+    for obj in queryset:
+        carga_masiva_evidencias.delay(obj.id,request.user.id)
+masivo.short_description = 'Ejecutar carga masiva'
+
+class CargaMasivaAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    ordering = ['id']
+    actions = [masivo]
+
+admin.site.register(CargaMasiva,CargaMasivaAdmin)
