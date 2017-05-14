7729c28323fcfeeb3dbdd389ac6bc4aeea1a1674
Dandresfs <dandresfsoto@gmail.com>
2017-05-13 20:13:33 -0500

    TelegramBOT permabots v6

--- sican/settings/base.py
+++ sican/settings/base.py
@@ -156,9 +156,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
     'PAGE_SIZE': 10,
     'UNICODE_JSON': True
 }
